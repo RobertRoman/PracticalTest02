cfa99ec56bbf500771f14055d78f151a490d949a
robert <robertroman13@yahoo.com>
2020-05-18 09:32:12 +0300

    subiectele 3,4,5,6

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/CommunicationThread.java
@@ -0,0 +1,136 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.ResponseHandler;
+import cz.msebera.android.httpclient.client.entity.UrlEncodedFormEntity;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.impl.client.BasicResponseHandler;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.message.BasicNameValuePair;
+import cz.msebera.android.httpclient.protocol.HTTP;
+import cz.msebera.android.httpclient.util.EntityUtils;
+
+public class CommunicationThread extends Thread {
+
+    private ServerThread serverThread;
+    private Socket socket;
+
+    public CommunicationThread(ServerThread serverThread, Socket socket) {
+        this.serverThread = serverThread;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        if (socket == null) {
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] Socket is null!");
+            return;
+        }
+        try {
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            PrintWriter printWriter = Utilities.getWriter(socket);
+            if (bufferedReader == null || printWriter == null) {
+                Log.e(Constants.TAG, "[COMMUNICATION THREAD] Buffered Reader / Print Writer are null!");
+                return;
+            }
+            Log.i(Constants.TAG, "[COMMUNICATION THREAD] Waiting for parameters from client curs type!");
+            String informationType = bufferedReader.readLine();
+
+            String data = serverThread.getData();
+            String cursInformation = null;
+
+            //TODO 1 min check
+//            if (data.containsKey(city)) {
+//                Log.i(Constants.TAG, "[COMMUNICATION THREAD] Getting the information from the cache...");
+//                weatherForecastInformation = data.get(city);
+//            } else
+            {
+                Log.i(Constants.TAG, "[COMMUNICATION THREAD] Getting the information from the webservice...");
+                HttpClient httpClient = new DefaultHttpClient();
+                String pageSourceCode = "";
+
+                HttpGet httpGet = new HttpGet(Constants.WEB_SERVICE_ADDRESS + informationType + ".json");
+                Log.i("Test", httpGet.toString());
+                HttpResponse httpGetResponse = httpClient.execute(httpGet);
+                HttpEntity httpGetEntity = httpGetResponse.getEntity();
+                if (httpGetEntity != null) {
+                    pageSourceCode = EntityUtils.toString(httpGetEntity);
+
+                }
+
+
+                if (pageSourceCode == null) {
+                    Log.e(Constants.TAG, "[COMMUNICATION THREAD] Error getting the information from the webservice!");
+                    return;
+                } else
+                    Log.i(Constants.TAG, pageSourceCode);
+
+
+                JSONObject content = new JSONObject(pageSourceCode);
+                JSONObject currencies = content.getJSONObject("bpi");
+                JSONObject money = currencies.getJSONObject(informationType);
+                cursInformation = money.get("rate").toString();
+
+
+                Log.i("Test", cursInformation);
+                //String temperature = main.getString(Constants.TEMP);
+
+                serverThread.setData(cursInformation);
+
+                if (cursInformation == null) {
+                    Log.e(Constants.TAG, "[COMMUNICATION THREAD] Curs data is null!");
+                    return;
+                }
+            }
+
+
+            printWriter.println(cursInformation);
+            printWriter.flush();
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        } catch (JSONException jsonException) {
+            Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + jsonException.getMessage());
+            if (Constants.DEBUG) {
+                jsonException.printStackTrace();
+            }
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException ioException) {
+                    Log.e(Constants.TAG, "[COMMUNICATION THREAD] An exception has occurred: " + ioException.getMessage());
+                    if (Constants.DEBUG) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+}
--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -3,12 +3,104 @@ package ro.pub.cs.systems.eim.practicaltest02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PracticalTest02MainActivity extends AppCompatActivity {
+
+    // Server widgets
+    private EditText serverPortEditText = null;
+    private Button connectButton = null;
+
+    // Client widgets
+    private EditText clientAddressEditText = null;
+    private EditText clientPortEditText = null;
+    private Spinner informationTypeSpinner = null;
+    private Button cursButton = null;
+    private TextView cursTextView = null;
+
+    private ServerThread serverThread = null;
+    private ClientThread clientThread = null;
+
+
+    private ConnectButtonClickListener connectButtonClickListener = new ConnectButtonClickListener();
+    private class ConnectButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            String serverPort = serverPortEditText.getText().toString();
+            if (serverPort == null || serverPort.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] Server port should be filled!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            serverThread = new ServerThread(Integer.parseInt(serverPort));
+            if (serverThread.getServerSocket() == null) {
+                Log.e(Constants.TAG, "[MAIN ACTIVITY] Could not create server thread!");
+                return;
+            }
+            serverThread.start();
+        }
+
+    }
+
+    private GetCursButtonClickListener getCursButtonClickListener = new GetCursButtonClickListener();
+    private class GetCursButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            String clientAddress = clientAddressEditText.getText().toString();
+            String clientPort = clientPortEditText.getText().toString();
+            if (clientAddress == null || clientAddress.isEmpty()
+                    || clientPort == null || clientPort.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] Client connection parameters should be filled!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (serverThread == null || !serverThread.isAlive()) {
+                Toast.makeText(getApplicationContext(), "[MAIN ACTIVITY] There is no server to connect to!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            String informationType = informationTypeSpinner.getSelectedItem().toString();
+            Log.i("Test",informationType);
+            cursTextView.setText(Constants.EMPTY_STRING);
+
+            clientThread = new ClientThread(
+                    clientAddress, Integer.parseInt(clientPort), informationType, cursTextView
+            );
+            clientThread.start();
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        serverPortEditText = (EditText)findViewById(R.id.server_port_edit_text);
+        connectButton = (Button)findViewById(R.id.connect_button);
+        connectButton.setOnClickListener(connectButtonClickListener);
+
+        clientAddressEditText = (EditText)findViewById(R.id.client_address_edit_text);
+        clientPortEditText = (EditText)findViewById(R.id.client_port_edit_text);
+
+        informationTypeSpinner = (Spinner)findViewById(R.id.information_type_spinner);
+        cursButton = (Button)findViewById(R.id.get_curs_button);
+        cursButton.setOnClickListener(getCursButtonClickListener);
+        cursTextView = (TextView)findViewById(R.id.curs_text_view);
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(Constants.TAG, "[MAIN ACTIVITY] onDestroy() callback method has been invoked");
+        if (serverThread != null) {
+            serverThread.stopThread();
+        }
+        super.onDestroy();
     }
 }
